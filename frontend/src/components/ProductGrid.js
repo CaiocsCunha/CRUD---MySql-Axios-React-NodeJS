102610eb23a48422a0a9f682c10b26876674607b
CaiocsCunha <caiocscunha@gmail.com>
2023-03-21 23:22:01 -0300

    Inicio Carrinho de Compras

--- frontend/src/components/ProductGrid.js
+++ frontend/src/components/ProductGrid.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { toast } from 'react-toastify'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -20,6 +21,7 @@ const ItemContainer = styled.div`
   align-items: center;
   justify-content: center;
   gap: 10px;
+  cursor: pointer;
 
   &:hover {
     background-color: palevioletred;
@@ -37,12 +39,37 @@ const GridContainer = styled.div`
 `
 
 const ProductGrid = ({ products }) => {
+  const carrinho = []
+  function handleClick(e) {
+    e.preventDefault()
+    let itemCarrinho = {
+      id: undefined,
+      nome: undefined,
+      quantidade: undefined
+    }
+    let itemSelecionado = products.filter(item => item.id == e.target.id)[0]
+    const found = carrinho.find(element => element.id == itemSelecionado.id)
+
+    if (found) {
+      let indexUpdate = carrinho.findIndex(item => item.id == found.id)
+      carrinho[indexUpdate].quantidade += 1
+    } else {
+      itemCarrinho.id = itemSelecionado.id
+      itemCarrinho.nome = itemSelecionado.nome
+      if (itemCarrinho.quantidade == undefined) itemCarrinho.quantidade = 1
+      else itemCarrinho.quantidade += 1
+
+      carrinho.push(itemCarrinho)
+    }
+    toast.success('Adicionado ao carrinho!')
+  }
+
   return (
     <Container>
       <h2>PRODUTOS</h2>
       <GridContainer>
-        {products.map((item, i) => (
-          <ItemContainer key={i}>
+        {products.map(item => (
+          <ItemContainer onClick={handleClick} key={item.id} id={item.id}>
             <div>{item.nome}</div>
             <div>R$ {item.preco}</div>
           </ItemContainer>
